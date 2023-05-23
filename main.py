1bb96b57cdce9a6719b4bc4df873d681ca3d25c3
kaushal kailas kotkar <kaushalkotkar9@gmail.com>
2023-05-23 13:00:49 +0530

    firstcommit

---
+++ main.py
@@ -0,0 +1,14 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+list1 = [0,2,6]
+
+for i in list1:
+    if i == 0:
+        print("Even")
+    elif i == 2:
+        print("Even")
+    else:
+        print("Odd")
